73161ed49c62cd002709921dbcae5932e060aaac
thannous <thannous@gmail.com>
2013-02-02 04:36:22 +0100

    ajout map

--- src/com/datahack/lastmetro/MainActivity.java
+++ src/com/datahack/lastmetro/MainActivity.java
@@ -1,15 +1,25 @@
 package com.datahack.lastmetro;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	  static final LatLng KIEL = new LatLng(53.551, 9.993);
+	  private GoogleMap map;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		 map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+			        .getMap();
+		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
 	}
 
 	@Override
